32ca82ed462d55500adf16cdd43171da455dc24d
hk222gn <hk222gn>
2013-11-26 20:34:54 +0100

    Börjat på whitebox testning

---
+++ Whitebox/UnitTestTriangel/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTriangel
+{
+    [TestClass]
+    public class TriangleUnitTest
+    {
+        //likbent
+        [TestMethod]
+        public void isIsosceles()
+        {
+            Triangle t = new Triangle(1.0, 3.0, 1.0);
+            Assert.IsTrue(t.isIsosceles());
+        }
+        //liksidig
+        [TestMethod]
+        public void isEquilateral()
+        {
+            Triangle t = new Triangle(2.0, 2.0, 2.0);
+            Assert.IsTrue(t.isEquilateral());
+        }
+        //Inga lika sidor
+        [TestMethod]
+        public void isScalene()
+        {
+            Triangle t = new Triangle(3.0, 4.0, 5.0);
+            Assert.IsTrue(t.isScalene());
+        }
+    }
+}
